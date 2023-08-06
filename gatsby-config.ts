5329297bc3096f5d7e68db56da4751989a3688b1
Josh Katz <josh@xqmsg.com>
2023-08-06 10:53:26 -0400

    lowercase font

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Personal site `,
+    title: `Joshua Katz`,
     siteUrl: `https://www.joshuaskatz.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
