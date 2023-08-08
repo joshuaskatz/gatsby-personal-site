31a6f45659095d097a61b81b68b046f578e5c4a2
Josh Katz <josh@xqmsg.com>
2023-08-08 11:53:23 -0400

    animations

--- src/components/image/ImageText.tsx
+++ src/components/image/ImageText.tsx
@@ -1,19 +1,11 @@
-import React from "react";
+import React from 'react'
 
-export default function ImageText({
-  title,
-  description,
-  sub,
-}: {
-  title: string;
-  description: string;
-  sub?: string;
-}) {
+export default function ImageText({ title, description, sub }: { title: string; description: string; sub?: string }) {
   return (
-    <div className="ml-2 mb-8 md:ml-5 w-fit">
+    <div className="mx-2 mb-8 md:mx-5 w-fit">
       <div className="text-5xl font-light">{title}</div>
       <div className="text-xl">{description}</div>
       {sub && <div className="text-lg">{sub}</div>}
     </div>
-  );
+  )
 }
--- src/components/software/index.tsx
+++ src/components/software/index.tsx
@@ -1,41 +1,41 @@
-import React, { PropsWithChildren, useRef, useState } from "react";
-import { FaChevronDown } from "react-icons/fa";
+import React, { PropsWithChildren, useEffect, useRef, useState } from 'react'
+import { FaChevronDown } from 'react-icons/fa'
+import { animated, useSpring } from 'react-spring'
 
-export default function SoftwareSection({
-  children,
-  section,
-}: PropsWithChildren & { section: string }) {
-  const [open, setOpen] = useState(true);
+export default function SoftwareSection({ children, section }: PropsWithChildren & { section: string }) {
+  const [open, setOpen] = useState(true)
 
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null)
+  const [contentMaxHeight, setContentMaxHeight] = useState(0)
+
+  useEffect(() => {
+    const calcContentMaxHeight = () => {
+      ref.current && setContentMaxHeight(ref.current.scrollHeight)
+    }
+
+    calcContentMaxHeight()
+
+    window.addEventListener('resize', () => calcContentMaxHeight())
+
+    return () => window.removeEventListener('resize', calcContentMaxHeight)
+  }, [ref, contentMaxHeight])
+
+  const props = useSpring({
+    opacity: open ? 1 : 0,
+    maxHeight: open ? `${contentMaxHeight}px` : '0px',
+    config: { duration: 300 },
+  })
 
   return (
-    <div id={section} className="w-full">
+    <div id={section} className="w-full h-fit">
       <div
-        onClick={() => {
-          setOpen(!open);
-
-          if (open && ref.current) {
-            window.scrollTo({
-              top: -ref.current.getBoundingClientRect().bottom,
-            });
-          }
-        }}
+        onClick={() => setOpen(!open)}
         className="p-2 w-full flex flex-row h-full items-center justify-between cursor-pointer"
       >
         <div className="text-4xl">{section}</div>
         <FaChevronDown size={28} />
       </div>
-      {open && (
-        <div
-          className={`transition-opacity duration-500 ${
-            open ? "opacity-100" : "opacity-0"
-          }`}
-          ref={ref}
-        >
-          {children}
-        </div>
-      )}
+      <div ref={ref}>{<animated.div style={{ ...props }}>{children}</animated.div>}</div>
     </div>
-  );
+  )
 }
